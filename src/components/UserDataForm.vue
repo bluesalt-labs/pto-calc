915fa3c2840dc693f35a82d29a71c56aff39ff99
Luke Sontrop <luke@bluesaltlabs.com>
2018-10-10 16:39:55 -0700

    adds a bunch of column and row container styling. Adds more user info fields

--- src/components/UserDataForm.vue
+++ src/components/UserDataForm.vue
@@ -1,8 +1,21 @@
 <template>
-  <form method="post" id="user-data-form">
-    <textarea>
-      {{ JSON.stringify(userData) }}
-    </textarea>
+  <form method="post" id="user-data-form" class="container-flex-row">
+
+    <div class="input-container container-quarter container-gutter">
+      <label for="start_date">Start Date</label>
+      <input type="text" id="start_date" name="start_date" v-model="userData.start_date" />
+    </div>
+
+    <div class="input-container container-quarter container-gutter">
+      <label for="start_hours">Initial PTO Hours</label>
+      <input type="text" id="start_hours" name="start_hours" v-model="userData.start_hours" />
+    </div>
+
+    <div class="input-container container-half container-gutter">
+      <label for="debug-output">Debug Output</label>
+      <textarea class="v-resize" id="debug-output" name="debug-output" rows="15">{{ formattedUserData }}</textarea>
+    </div>
+
   </form>
 </template>
 
@@ -13,6 +26,11 @@
     data() {
       return {
         userData: userdata
+      }
+    },
+    computed: {
+      formattedUserData: function() {
+        return JSON.stringify(this.userData, null, "\t");
       }
     }
   }
