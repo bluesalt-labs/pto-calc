8b72bd10fc21bb13d440fe9f73e45b609eea0b25
Luke Sontrop <luke@bluesaltlabs.com>
2018-10-11 17:00:52 -0700

    Gets the initial HoursTable working

--- src/components/HoursTable.vue
+++ src/components/HoursTable.vue
@@ -14,9 +14,9 @@
         :class="( rowData.is_add ? (rowData.is_future ? 'date-row-future' : 'date-row-past') : 'date-row-subtract')"
       >
         <td>{{ index + 1}}</td>
-        <td>{{ rowData.date }}</td>
+        <td>{{ rowData.date.format(dateFormat) }}</td>
         <td>{{ rowData.hours }}</td>
-        <td>{{ rowData.hours / 8 }}</td>
+        <td>{{ (rowData.hours / 8).toFixed(2) }}</td>
       </tr>
     </tbody>
   </table>
@@ -34,33 +34,58 @@
       dateFormat: {
         type: String,
         required: false,
-        default: "MM-DD-YYYY"
+        default: "MMMM Do YYYY"
       }
     },
     computed: {
       dateRows: function() {
         var output = [];
-        var currentHours = this.userData.start_hours;
-        var hoursPerDate = this.userData.hours_per_period;
 
-        var hoursUsedDates = this.getHoursUsed();
-        var payDates = this.getDateValues();
-
+        var usedDates = this.getHoursUsed();
+        var payDates  = this.getDateValues();
         var todayDate = moment();
 
+        var sortOutputCallback = function(a, b) {
+          if( a.date.isAfter(b.date)  ) { return 1; }
+          if( a.date.isBefore(b.date) ) { return -1; }
+          return 0;
+        };
+
+        // Add PTO dates
         for(var i in payDates) {
-          // todo: should subtract hours?
-          var payDate = moment(payDates[i]);
-
-
           output.push({
-            "date": payDate.format(this.dateFormat),
-            "hours": currentHours,
-            "is_future": (payDate > todayDate),
-            "is_add": true
-           });
+            "date":       payDates[i],
+            "hours":      0,
+            "is_future":  (payDates[i] > todayDate),
+            "is_add":     true
+          });
+        }
 
-          currentHours += hoursPerDate;
+        // Add PTO hours taken
+        for(var i in usedDates) {
+          output.push({
+            "date":       usedDates[i].date,
+            "hours":      usedDates[i].hours,
+            "is_future":  (usedDates[i] > todayDate),
+            "is_add":     false
+          });
+        }
+
+        if(output.length > 0) {
+          // Sort the output array before continuing
+          output.sort(sortOutputCallback);
+
+          var hoursPerDate = this.userData.hours_per_period;
+          var prevHours = this.userData.start_hours;
+
+          for(var i in output) {
+            if(output[i].is_add) {
+              output[i].hours = prevHours;
+              prevHours += hoursPerDate;
+            } else {
+              prevHours += output[i].hours;
+            }
+          }
         }
 
         return output;
@@ -76,7 +101,7 @@
         for(var rowDate = moment(startDate); rowDate <= endDate; rowDate.add(1, 'days')) {
           for(var i in this.userData.pay_period_days) {
             if(rowDate.date() === parseInt(this.userData.pay_period_days[i])) {
-              output.push( rowDate.format() );
+              output.push( moment(rowDate) );
             }
           }
         }
@@ -87,18 +112,10 @@
         var output = [];
 
         for(var i in this.userData.hours_used) {
-          var hours = 0;
-
-          try {
-            hours = parseInt(this.userData.hours_used[i].hours)
-          } catch(e) {}
-
-          if(hours > 0) {
-            output.push({
-              "date": moment(this.userData.hours_used[i].date),
-              "hours": hours
-            })
-          }
+          output.push({
+            "date": moment(this.userData.hours_used[i].date),
+            "hours": -Math.abs(parseInt(this.userData.hours_used[i].hours))
+          })
         }
 
         return output;
